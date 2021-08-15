d9aaca9dc8a54c47cfb63ee48f2db661998fd413
midorinoyama <fujiaqa@gmail.com>
2021-08-15 03:59:53 +0000

    [ADD]4-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+end
+
+
