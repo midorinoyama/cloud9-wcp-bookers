f79268ac9374ba58b754f41690564c688f7f463a
midorinoyama <fujiaqa@gmail.com>
2021-08-16 07:33:20 +0000

    [ADD]createバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def edit
